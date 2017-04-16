341def6387e0aaac360c210d4096321753309cf7
林博仁 <Buo.Ren.Lin@gmail.com>
2017-04-17 03:46:00 +0800

    .profile.d - 改善檔名
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- .profile.d/Apache Solr.source.sh
+++ .profile.d/Apache Solr.source.sh
@@ -1,13 +1,6 @@
 # 依照下列網頁說明設定 Nutch 跟 Solr 軟體的根目錄
 # http://wiki.apache.org/nutch/NutchTutorial#A4._Setup_Solr_for_search
-NUTCH_RUNTIME_HOME="$HOME/軟體/Apache_Nutch"
 APACHE_SOLR_HOME="${HOME}/軟體/Apache_Solr"
-
-if is_path_exist "${HOME}/軟體/Apache_Nutch" ; then
-	export NUTCH_RUNTIME_HOME
-else
-	unset NUTCH_RUNTIME_HOME
-fi
 
 if is_path_exist "${HOME}/軟體/Apache_Solr" ; then
 	export APACHE_SOLR_HOME
