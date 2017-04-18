3e79a8ba473a9dd7bcdb8bd4ff657b603dc0f6d3
林博仁 <Buo.Ren.Lin@gmail.com>
2017-04-18 14:11:56 +0800

    將 .profile.d 移動到子模組專案
    https://github.com/Lin-Buo-Ren/.profile.d.git
    
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- .profile.d/iStyle.source.sh
+++
@@ -1,1 +0,0 @@
-install_executable_search_path "${HOME}/軟體/iStyle - Fast and Free Automatic Formatter for Verilog Source Code/bin/release"
--- .profile.d/vdent.source.sh
+++
@@ -1,1 +0,0 @@
-install_executable_search_path "${HOME}/軟體/vdent: Verilog Indenter/master-c9817751/bin"
